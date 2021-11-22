250c6fff66f808b9fdc5b36dda2b658f7c8c39cf
adsionli <adsionli@foxmail.com>
2021-11-22 20:16:06 +0800

    排序代码

---
+++ normal/颜色分类75.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    let len = nums.length;
+    if (len == 0) {
+        return nums;
+    }
+    let L = 0, R = 1, target = R;
+    while (R < len) {
+        if (nums[L] > nums[target]) {
+            [nums[L], nums[target]] = [nums[target], nums[L]];
+            if (L == 0) {
+                L = R;
+                R++;
+                target = R;
+                continue
+            }
+            L--;
+            target--;
+        } else {
+            L = R;
+            R++;
+            target = R;
+        }
+    }
+
+    return nums;
+};
+
+console.log(sortColors([2, 0, 2, 1, 1, 0]))
