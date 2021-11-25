d21c7e1d17ba2fb9fd777c792d48491e74a47189
adsionli <adsionli@foxmail.com>
2021-11-25 22:55:18 +0800

    2021年11月25日每日三题完成

--- normal/从前序与中序遍历序列构造二叉树105.js
+++ normal/从前序与中序遍历序列构造二叉树105.js
@@ -11,32 +11,96 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
+// var buildTree = function (preorder, inorder) {
+//     let len = preorder.length;
+//     if (len == 1) {
+//         return new TreeNode(preorder[0])
+//     }
+//     let head = new TreeNode(preorder[0]);
+//     let map = new Map();
+//     for (let i = 0; i < len; i++) {
+//         map.set(preorder[i], i);
+//     }
+//     function find(arr) {
+//         let inD = Infinity;
+//         let d = null;
+//         for (let i = 0; i < arr.length; i++) {
+//             if (map.get(arr[i]) < inD) {
+//                 inD = map.get(arr[i]);
+//                 d = i;
+//             }
+//         }
+
+//         return arr[d];
+//     }
+//     function handle(inA, head) {
+//         let left = [];
+//         let right = [];
+//         let r = false, l = false;
+//         for (let i = 0; i < len; i++) {
+//             if (inA[i] == head.val) {
+//                 right = inA.slice(i + 1);
+//                 break;
+//             } else {
+//                 left.push(inA[i]);
+//             }
+//         }
+//         if (left.length > 1) {
+//             let nextHead = new TreeNode(find(left), null, null);
+//             head.left = nextHead;
+//             handle(left, nextHead)
+//         } else if (left.length == 1) {
+//             head.left = new TreeNode(left[0], null, null);
+//             l = true;
+//         } else if (left.length == 0) {
+//             l = true;
+//         }
+//         if (right.length > 1) {
+//             let nextHead = new TreeNode(find(right), null, null);
+//             head.right = nextHead;
+//             handle(right, nextHead)
+//         } else if (right.length == 1) {
+//             head.right = new TreeNode(right[0], null, null);
+//             r = true;
+//         } else if (right.length == 0) {
+//             r = true;
+//         }
+//         if(r && l){
+//             return ;
+//         }
+//     }
+//     handle(inorder, head);
+
+//     return head;
+// };
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
 var buildTree = function (preorder, inorder) {
     let len = preorder.length;
-    if(len == 1){
+    if (len == 1) {
         return new TreeNode(preorder[0])
     }
-    let head = new TreeNode(preorder[0]);
-    let left = [];
-    let right = [];
     let map = new Map();
     for (let i = 0; i < len; i++) {
-        if (inorder[i] == head.val) {
-            right = inorder.slice(i + 1);
-            break;
-        } else {
-            left.push(inorder[i]);
+        map.set(inorder[i], i);
+    }
+    function handle(pS, pE, iS, iE) {
+        if (pS === pE) {
+            return null;
         }
+        const rootVal = preorder[pS];
+        const root = new TreeNode(rootVal);
+
+        const irI = map.get(rootVal);
+        const lS = irI - iS;
+
+        root.left = handle(pS + 1, pS + lS + 1, iS, irI);
+        root.right = handle(pS + 1 + lS, pE, irI + 1, iE);
     }
-    for(let i = 0; i < len; i++){
-        map.set(preorder[i], i);
-    }
-    if(left.length == 0){
-        head.left == null;
-    }
-    if(right.length == 0){
-        head.right == null;
-    }
-    console.log(left)
-    console.log(right)
+
+
+    return handle(0, len, 0, len);
 };
