fb60f0956f7a27699efb51d6937c32e95085ffcc
adsionli <adsionli@foxmail.com>
2021-12-06 22:24:19 +0800

    每日一题打卡

---
+++ normal/超级次方372.js
@@ -0,0 +1,29 @@
+//TAG 快速幂
+/**
+ * @param {number} a
+ * @param {number[]} b
+ * @return {number}
+ */
+const MOD = 1337
+var superPow = function(a, b) {
+    dfs = function(idx) {
+        if(idx == -1)
+            return 1
+        return quickPow(dfs(idx-1), 10) * quickPow(a, b[idx]) % MOD
+    }
+
+    quickPow = function(x, y){
+        let ans = 1
+        x %= MOD
+        while(y != 0){
+            if((y & 1) != 0)
+                ans = ans * x % MOD
+            x = x * x % MOD
+            y >>= 1
+        }
+        return ans
+    }
+
+    a %= MOD
+    return dfs(b.length-1)
+};
