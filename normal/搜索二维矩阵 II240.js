37a01e2dc623443c04cb96aa11fc3b862c64acf2
adsionli <adsionli@foxmail.com>
2021-12-13 09:38:57 +0800

    搜索二维矩阵的第二种BST树模拟写法

--- normal/搜索二维矩阵 II240.js
+++ normal/搜索二维矩阵 II240.js
@@ -38,3 +38,28 @@ var searchMatrix = function (matrix, target) {
  * 即先列后行（先列的话需要先搜索两列，第一列和最后一列，这样能够保证先排除最大最小的不符合的，达到加速的目的）
  */
 console.log(searchMatrix([[-5]], -2));
+
+/**
+ * @description 实际上就是将这个矩阵看成是一个BST树，然后就是做一个BSt树的模拟。
+ * 那么我们可以从根（右上角）开始搜索，如果当前的节点不等于目标值，可以按照树的搜索顺序进行：
+    当前节点「大于」目标值，搜索当前节点的「左子树」，也就是当前矩阵位置的「左方格子」，即 c--
+    当前节点「小于」目标值，搜索当前节点的「右子树」，也就是当前矩阵位置的「下方格子」，即 r++
+ * @param {*} matrix 
+ * @param {*} target 
+ */
+var searchMatrixByBst = function (matrix, target) {
+    let n = matrix.length;
+    let m = matrix[0].length;
+    let i = 0;
+    let j = m - 1;
+    while (i >= 0 && i < n && j >= 0 && j < m) {
+        if (matrix[i][j] == target) {
+            return true;
+        }
+        matrix[i][j] > target ? j-- : i++;
+    }
+
+    return false;
+}
+
+console.log(searchMatrixByBst([[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 5));
