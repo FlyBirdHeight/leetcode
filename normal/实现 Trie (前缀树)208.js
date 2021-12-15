f3ecf838edd21c8655cb981c87bc389d26c2c3b3
adsionli <adsionli@foxmail.com>
2021-12-16 00:12:46 +0800

    字典树

---
+++ normal/实现 Trie (前缀树)208.js
@@ -0,0 +1,49 @@
+var Trie = function() {
+    this.children = {};
+};
+
+Trie.prototype.insert = function(word) {
+    let node = this.children;
+    for (const ch of word) {
+        if (!node[ch]) {
+            node[ch] = {};
+        }
+        node = node[ch];
+    }
+    console.log(this.children)
+    node.isEnd = true;
+};
+
+Trie.prototype.searchPrefix = function(prefix) {
+    let node = this.children;
+    for (const ch of prefix) {
+        if (!node[ch]) {
+            return false;
+        }
+        node = node[ch];
+    }
+    return node;
+}
+
+Trie.prototype.search = function(word) {
+    const node = this.searchPrefix(word);
+    return node !== undefined && node.isEnd !== undefined;
+};
+
+Trie.prototype.startsWith = function(prefix) {
+    return this.searchPrefix(prefix);
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
+
+var obj = new Trie();
+obj.insert("apple");
+// obj.insert("app");
+// obj.insert("add");
+console.log(obj.search('apple'));
