9f0601f20fb060cc38281074b524dee5077fa22c
adsionli <adsionli@foxmail.com>
2021-12-24 14:30:54 +0800

    在做一题

--- normal/完全平方数279.js
+++ normal/完全平方数279.js
@@ -2,14 +2,30 @@
  * @param {number} n
  * @return {number}
  */
-var numSquares = function(n) {
-    let m = [];
-    let ans = 1;
-    while(Math.pow(ans, 2) < 10000){
-        m.push(Math.pow(ans, 2));
-        ans++;
+var numSquares = function (n) {
+    let res = 0;
+    let q = [n];
+    let v = new Set();
+    while (q.length != 0) {
+        let len = q.length;
+        res++;
+        for (let i = 0; i < len; i++) {
+            let data = q.shift();
+            for (let j = 1; j * j <= data; j++) {
+                let resData = data - Math.pow(j, 2);
+                if (resData == 0) {
+                    return res;
+                }
+                if (v.has(resData)) {
+                    continue;
+                }
+                v.add(resData);
+                q.push(resData);
+            }
+        }
     }
-    console.log(m)
+
+    return res;
 };
 
-console.log(numSquares(123));
+console.log(numSquares(13));
