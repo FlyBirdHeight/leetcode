2825660ab6cb01210fa3a96ebceeec7f6fac7442
adsionli <adsionli@foxmail.com>
2022-01-23 23:56:40 +0800

    明天序列化二叉树

---
+++ 剑指offer(第二版)/37_序列化二叉树.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function (root) {
+    if(!root){
+        return "[]";
+    }
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function (data) {
+    if(data.length == 2){
+        return null;
+    }
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
